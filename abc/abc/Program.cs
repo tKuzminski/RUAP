c814869e34c3a5db0dc89cba11f06ec1823daf2b
kuzminski.lucija@gmail.com <student@iMac-6.local>
2024-11-11 09:39:49 +0100

    pppp

--- abc/abc/Program.cs
+++ abc/abc/Program.cs
@@ -6,7 +6,7 @@ namespace abc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("asdfgh");
         }
     }
 }
